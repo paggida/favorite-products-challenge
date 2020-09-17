4d2912384a355dd8d12c3500f91d23b38c4bef94
paggida <danilopaggi@gmail.com>
2020-09-17 08:58:29 -0300

    Rename the swagger file [02]

--- src/shared/infra/http/api/v1.ts
+++ src/shared/infra/http/api/v1.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
-import swaggerDocument from './v1.json';
-import clientRouter from '@modules/users/infra/http/routes/client';
+import swaggerDocument from './swagger.v1.json';
+import clientRouter from '@modules/client/infra/http/routes/client';
 
 const v1Router = Router();
 
-v1Router.use('/v1/clients', clientRouter);
-v1Router.use('/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+v1Router.use('/v1/client', clientRouter);
+v1Router.get('/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default v1Router;
