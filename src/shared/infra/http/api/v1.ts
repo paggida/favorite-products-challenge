8e8ed5b94e65d67e60a29ebf690959b43b7845eb
paggida <danilopaggi@gmail.com>
2020-09-18 19:48:48 -0300

    Base documentation for Client & Product routes [2]

--- src/shared/infra/http/api/v1.ts
+++ src/shared/infra/http/api/v1.ts
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerDocument from './swagger.v1.json';
-import clientRouter from '@modules/client/infra/http/routes/clientRouter';
 import sessionRouter from '@modules/session/infra/http/routes/sessionRouter';
+import clientRouter from '@modules/client/infra/http/routes/clientRouter';
+import productRouter from '@modules/product/infra/http/routes/productRouter';
 
 const v1Router = Router();
 
-v1Router.use('/v1/client', clientRouter);
 v1Router.use('/v1/session', sessionRouter);
+v1Router.use('/v1/client', clientRouter);
+v1Router.use('/v1/product', productRouter);
 v1Router.use('/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default v1Router;
