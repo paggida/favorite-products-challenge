19b78edc5e1af90a65e939c0b795208cd710347d
paggida <danilopaggi@gmail.com>
2020-09-19 17:15:28 -0300

    Library nomenclature change

--- src/modules/session/infra/http/middlewares/ensureBodyInput.ts
+++ src/modules/session/infra/http/middlewares/ensureBodyInput.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
-import * as Yup from 'yup';
+import * as yup from 'yup';
+
 import AccessCodeBodyError from '@modules/session/errors/AccessCodeBodyError';
 
 export default async function ensureBodyInput(req: Request, res: Response, next: NextFunction): Promise<object | void> {
-  const schema = Yup.object().shape({
-    accessCode: Yup.string().strict(true).required(),
+  const schema = yup.object().shape({
+    accessCode: yup.string().strict(true).required(),
   });
 
   if (!(await schema.isValid(req.body))) {
