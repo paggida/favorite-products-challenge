94d201f206d60a3bead87e4e555931cd3c99ad8d
paggida <danilopaggi@gmail.com>
2020-09-20 22:19:07 -0300

    Reorganization of imports

--- src/modules/session/services/AuthenticateService.spec.ts
+++ src/modules/session/services/AuthenticateService.spec.ts
@@ -5,11 +5,12 @@ import authConfig from '@config/auth';
 import AccessCodeAuthenticityError from '@modules/session/errors//AccessCodeAuthenticityError'
 import AuthenticateService from '@modules/session/services/AuthenticateService';
 
+const authService = container.resolve(AuthenticateService);
+
 describe('Validation of Authenticate Service.', () => {
   it('Should be able to get a JWT token with an valid access code.', async () => {
     let jwtVerify: boolean = false;
 
-    const authService = container.resolve(AuthenticateService);
     const authServiceResponse = await authService.execute({ accessCode: authConfig.accessCode});
     const { token } = authServiceResponse;
 
@@ -29,7 +30,6 @@ describe('Validation of Authenticate Service.', () => {
     let isIntanceOfAccessCodeAuthenticityError: boolean = false;
 
     try{
-      const authService = container.resolve(AuthenticateService);
       await authService.execute({ accessCode: 'Jest_TestAccessCode' });
     }catch(err){
       isErrorHappened = true;
